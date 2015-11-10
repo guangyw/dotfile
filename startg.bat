190e11d884eb89b1da2ba808216c49c777168f7a
Guangyan Wang <wanggyy@gmail.com>
2015-11-09 17:21:11 -0800

    add startg.bat

---
+++ startg.bat
@@ -0,0 +1,15 @@
+C:
+
+cd C:\cygwin64\bin
+
+set DISPLAY=:0.0
+
+set PATH=%PATH%:/usr/bin
+
+bash.exe /usr/bin/startg
+
+set LANG=en_US.UTF8
+
+gnome-terminal.exe
+
+echo Now You can Close the Cmd Window
